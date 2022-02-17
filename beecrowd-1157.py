2d17ef341fffbe2ed1a91bec584bfd305d8cce42
Jéssica Bessa de Oliveira <jessicabessaoliveira@gmail.com>
2022-02-17 11:00:26 -0300

    Create beecrowd-1157.py

---
+++ beecrowd-1157.py
@@ -0,0 +1,16 @@
+# https://www.beecrowd.com.br/judge/pt/problems/view/1157
+
+'''
+Ler um número inteiro N e calcular todos os seus divisores.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Escreva todos os divisores positivos de N, um valor por linha.
+'''
+
+n = int(input())
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i)
