e24a1e74aa5cf275dbb3cb3a7a8fe94c886ef667
Jéssica Bessa de Oliveira <jessicabessaoliveira@gmail.com>
2022-02-17 11:00:20 -0300

    Create beecrowd-1153.py

---
+++ beecrowd-1153.py
@@ -0,0 +1,16 @@
+# https://www.beecrowd.com.br/judge/pt/problems/view/1153
+
+'''
+Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+
+Entrada
+A entrada contém um valor inteiro N (0 < N < 13).
+
+Saída
+A saída contém um valor inteiro, correspondente ao fatorial de N.
+'''
+num = int(input())
+fat = 1
+for i in range(1, num + 1):
+    fat = fat * i
+print(f'{fat}')
